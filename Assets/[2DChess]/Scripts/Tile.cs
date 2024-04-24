e4c6c1af66cdb2cc0433a141830d7d571e621bc1
Anastasya Duygu Kılıç <anastasyaduygu@gmail.com>
2024-04-24 17:53:46 +0300

    (script) update : Tile script, added hover highlight on tiles

--- Assets/[2DChess]/Scripts/Tile.cs
+++ Assets/[2DChess]/Scripts/Tile.cs
@@ -4,8 +4,23 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private Color baseColor, offsetColor;
     [SerializeField] private SpriteRenderer spriteRenderer;
+    
+    [SerializeField] private GameObject hover,selected,selectable;
+
+    public bool isSelected = false;
     public void Init(bool isOffset)
     {
         spriteRenderer.color = isOffset ? offsetColor : baseColor;
+    }
+    private void OnMouseEnter()
+    {
+        if (!isSelected)
+        {
+            hover.SetActive(true);
+        }
+    }
+    private void OnMouseExit()
+    {
+        hover.SetActive(false);
     }
 }
