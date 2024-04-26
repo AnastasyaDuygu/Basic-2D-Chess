371c4c85edc48d5ed7b6b6a3d1c5c6bedda2cf04
Anastasya Duygu Kılıç <anastasyaduygu@gmail.com>
2024-04-26 17:47:03 +0300

    (script) update : Piece, diffColor bool func added

--- Assets/[2DChess]/Scripts/Pieces/Piece.cs
+++ Assets/[2DChess]/Scripts/Pieces/Piece.cs
@@ -5,14 +5,15 @@ public class Piece : MonoBehaviour
     public int color = 0; //1:white 2:black
     public bool isFirstMove = true; //for pawns, set false after first move
 
-    public virtual void HighlightSelectable(int x, int y, Tile[,] tileArray)
-    {
-        Debug.Log("BASE");
-    }
+    public virtual void HighlightSelectable(int x, int y, Tile[,] tileArray) { }
     
     public bool isPieceInTile(Tile tile) 
     {
         return tile.holdedPiece != null;
-       
+    }
+    public bool diffColor(Tile tile, int color)
+    {
+        if (tile.holdedPiece.color != color) return true;
+        return false;
     }
 }
