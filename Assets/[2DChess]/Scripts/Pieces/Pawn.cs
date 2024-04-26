8f09f28fe769e64c41bbfd6badad9f07a188179a
Anastasya Duygu Kılıç <anastasyaduygu@gmail.com>
2024-04-26 17:47:52 +0300

    (scripts) update : Pawn & Rook scripts override func for HighlightSelectable

--- Assets/[2DChess]/Scripts/Pieces/Pawn.cs
+++ Assets/[2DChess]/Scripts/Pieces/Pawn.cs
@@ -5,6 +5,66 @@ public class Pawn : Piece
 {
     public override void HighlightSelectable(int x, int y, Tile[,] tileArray)
     {
-        Debug.Log("Selectable Pawn");
+        int rightx, leftx, yafter;
+        //check color
+        if (color == 1) //white
+        {
+            yafter = y + 1;
+            rightx = x + 1; //check if there is piece infront it can eat
+            leftx = x - 1;
+        }
+        else if (color == 2) //black
+        {
+            yafter = y - 1;
+            rightx = x + 1;
+            leftx = x - 1;
+        }
+        else
+        {
+            rightx = -1;
+            leftx = -1;
+            yafter = -1;
+            Debug.LogError("invalid color" + color);
+        }
+        if (x < 8 && x >= 0 && yafter < 8 && yafter >= 0)
+        {
+            if (!isPieceInTile(tileArray[x, yafter]))
+            {
+                tileArray[x, yafter].SelectableHighlight();
+            }
+        }
+        //for taking pieces
+        if (rightx < 8 && rightx >= 0 && yafter < 8 && yafter >= 0)
+        {
+            if (isPieceInTile(tileArray[rightx, yafter]) && diffColor(tileArray[rightx,yafter], color)) 
+            {
+                Debug.Log("PIECE RIGHT");
+                tileArray[rightx, yafter].SelectableHighlight();
+            }
+        }
+        if (leftx < 8 && leftx >= 0 && yafter < 8 && yafter >= 0)
+        {
+            if (isPieceInTile(tileArray[leftx, yafter]) && diffColor(tileArray[leftx, yafter], color))
+            {
+                Debug.Log("PIECE LEFT");
+                tileArray[leftx, yafter].SelectableHighlight();
+            }
+        }
+        
+        //if first move
+        if (isFirstMove && !isPieceInTile(tileArray[x,yafter]))
+        {
+            if (color == 1)
+                yafter++; //white
+            else yafter--; //black
+            
+            if (x < 8 && x >= 0 && yafter < 8 && yafter >= 0)
+            {
+                if (!isPieceInTile(tileArray[x, yafter]))
+                {
+                    tileArray[x, yafter].SelectableHighlight();
+                }
+            }
+        }
     }
 }
--- Assets/[2DChess]/Scripts/Pieces/Rook.cs
+++ Assets/[2DChess]/Scripts/Pieces/Rook.cs
@@ -6,5 +6,7 @@ public class Rook : Piece
     public override void HighlightSelectable(int x, int y, Tile[,] tileArray)
     {
         Debug.Log("Selectable Rook");
+        //check color
+        //Debug.Log("Color : " + color);
     }
 }
