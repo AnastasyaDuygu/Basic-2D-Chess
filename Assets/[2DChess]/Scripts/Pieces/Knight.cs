312b887529868ddf3dd7fbad267eb2dd25a79ed7
Anastasya Duygu Kılıç <anastasyaduygu@gmail.com>
2024-04-29 14:00:17 +0300

    (script) update : Knight script,  implemented HighlightSelectable method for the Knight pieces

--- Assets/[2DChess]/Scripts/Pieces/Knight.cs
+++ Assets/[2DChess]/Scripts/Pieces/Knight.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Knight : Piece
@@ -6,5 +5,21 @@ public class Knight : Piece
     public override void HighlightSelectable(int x, int y, Tile[,] tileArray)
     {
         Debug.Log("Selectable Knight");
+        
+        int[] X = { 2, 1, -1, -2, -2, -1, 1, 2 };
+        int[] Y = { 1, 2, 2, 1, -1, -2, -2, -1 };
+        for (int i = 0; i < 8; i++)
+        {
+            int xafter = x + X[i];
+            int yafter = y + Y[i];
+            
+            if (xafter < 8 && xafter >= 0 && yafter < 8 && yafter >= 0)
+            {
+                if (!isPieceInTile(tileArray[xafter, yafter]) || diffColor(tileArray[xafter, yafter], color))
+                {
+                    tileArray[xafter, yafter].SelectableHighlight();
+                }
+            }
+        }
     }
 }
