92e3d46bb56d6c454fe4a0f093b04271eaa2fa86
Anastasya Duygu Kılıç <anastasyaduygu@gmail.com>
2024-04-29 14:04:59 +0300

    (script) update : Bishop script, implemented HighlightSelectable method for the Bishop pieces

--- Assets/[2DChess]/Scripts/Pieces/Bishop.cs
+++ Assets/[2DChess]/Scripts/Pieces/Bishop.cs
@@ -1,10 +1,36 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bishop : Piece
 {
     public override void HighlightSelectable(int x, int y, Tile[,] tileArray)
     {
-        Debug.Log("Selectable Bishop");
+        //Debug.Log("Selectable Bishop");
+        
+        int[] X = { -1, 1, -1, 1 };
+        int[] Y = { 1, 1, -1, -1 };
+
+        for (int i = 0; i < 4; i++)
+        {
+            int xafter = x;
+            int yafter = y;
+
+            for (int j = 0; j < 8; j++)
+            {
+                xafter += X[i];
+                yafter += Y[i];
+
+                if (xafter < 8 && xafter >= 0 && yafter < 8 && yafter >= 0)
+                {
+                    if (!isPieceInTile(tileArray[xafter, yafter]))
+                        tileArray[xafter, yafter].SelectableHighlight();
+                    else if (diffColor(tileArray[xafter, yafter], color))
+                    {
+                        tileArray[xafter, yafter].SelectableHighlight();
+                        break;
+                    }
+                    else break;
+                }
+            }
+        }
     }
 }
