9a9e4c44a41fa247d9d08addade11969ef512266
Anastasya Duygu Kılıç <anastasyaduygu@gmail.com>
2024-04-29 14:12:54 +0300

    (script) update : King script, implemented HighlightSelectable method for the King pieces

--- Assets/[2DChess]/Scripts/Pieces/King.cs
+++ Assets/[2DChess]/Scripts/Pieces/King.cs
@@ -1,10 +1,21 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class King : Piece
 {
     public override void HighlightSelectable(int x, int y, Tile[,] tileArray)
     {
-        Debug.Log("Selectable King");
+        //Debug.Log("Selectable King");
+        
+        int[] X = { -1, 1, -1, 1, 0, 0, -1, 1 };
+        int[] Y = { 1, 1, -1, -1, 1, -1, 0, 0 };
+        for (int i = 0; i < 8; i++)
+        {
+            int xafter = x + X[i];
+            int yafter = y + Y[i];
+            
+            if (xafter < 8 && xafter >= 0 && yafter < 8 && yafter >= 0)
+                if (!isPieceInTile(tileArray[xafter, yafter]) || diffColor(tileArray[xafter, yafter], color)) 
+                    tileArray[xafter, yafter].SelectableHighlight();
+        }
     }
 }
