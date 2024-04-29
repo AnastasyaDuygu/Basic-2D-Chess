00695673231319ed2d9a074f49b8a253303aeb05
Anastasya Duygu Kılıç <anastasyaduygu@gmail.com>
2024-04-29 11:49:11 +0300

    (script) update : PieceMovement script, implemented MovePiece method

--- Assets/[2DChess]/Scripts/PieceMovement.cs
+++ Assets/[2DChess]/Scripts/PieceMovement.cs
@@ -2,10 +2,25 @@ using UnityEngine;
 
 public class PieceMovement : MonoBehaviour
 {
-    public void MovePiece()
+    public void MovePiece(Tile currentlySelectedTile, Tile clickedTile, TileManager tileManager)
     {
         Debug.Log("Piece Moved");
-        //switch turn
+        
         //deselect all highlights & isSelectable/isSelected
+        if (currentlySelectedTile.holdedPiece.isFirstMove == true)
+        {
+            currentlySelectedTile.holdedPiece.isFirstMove = false;
+        }
+        
+        Piece piece = currentlySelectedTile.holdedPiece;
+        //change transform of piece to the transform of clicked tile
+        piece.transform.position = clickedTile.transform.position;
+        piece.transform.parent = clickedTile.transform; //change parent of piece
+        
+        clickedTile.holdedPiece = piece;
+        currentlySelectedTile.holdedPiece = null;
+        
+        //switch turn
+        tileManager.SwitchTurn();
     }
 }
